182477fa548bc612eb16d1c6b407e5d80042e0aa
bhuvy <bhuvan.venkatesh21@gmail.com>
2018-12-02 01:11:10 -0600

    Getting router working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,14 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
-
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <BrowserRouter>
+	<Switch>
+        <Route path="/" component={()=><h1>index</h1>}/>
+        <Route path="/hello" component={()=><h1>hello</h1>}/>
+	</Switch>
+      </BrowserRouter>
     );
   }
 }
