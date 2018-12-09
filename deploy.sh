4f0d79b81ba5f4e8008f81ca0bcd4400cfb2634a
bhuvy <bhuvan.venkatesh21@gmail.com>
2018-12-08 22:50:09 -0600

    Trying it out

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e;
+
+TMP_DIR=`mktemp -d`
+cp ./build $TMP_DIR;
+
+pushd $TMP_DIR;
+
+git init
+git remote add origin git@github.com:bhuvy2/bhuvy2.github.io.git
+git add -A
+git commit -m "Deploying to site"
+git push origin master --force
+
+popd;
