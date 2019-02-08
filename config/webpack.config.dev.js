3e7b2d6093d72ec909d2392294ea0ecead7d75e3
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-02-08 13:08:13 -0600

    nohash

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -205,7 +205,7 @@ module.exports = {
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
-              name: 'static/media/[name].[hash:8].[ext]',
+              name: 'static/media/[name].[ext]',
             },
           },
             {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -278,7 +278,7 @@ module.exports = {
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
-              name: 'static/media/[name].[hash:8].[ext]',
+              name: 'static/media/[name].[ext]',
             },
           },
             {
